9434298f54d6fecfcc08a2d4873d61331877eda4
MohameddAshraff89 <mohameddashraff69@gmail.com>
2023-11-23 01:22:25 +0200

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,8 +9,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  platHome =
-    'assets/Images/homeImages/WhatsApp Image 2023-11-10 at 20.01.58_0cac2f19.jpg';
-  carousel1 = 'assets/Images/homeImages/carousel1.png';
-  carousel2 = 'assets/Images/homeImages/carousel2.png';
+  hollmannHome = 'assets/Images/homeImages/carousel0.jpg';
+  carousel1 = 'assets/Images/homeImages/carousel1.jpg';
+  carousel2 = 'assets/Images/homeImages/carousel2.jpg';
+  carousel3 = 'assets/Images/homeImages/carousel3.jpg';
+
+  images = [
+    'assets/Images/homeImages/carousel0.jpg',
+    'assets/Images/homeImages/carousel1.jpg',
+    'assets/Images/homeImages/carousel2.jpg',
+    'assets/Images/homeImages/carousel3.jpg',
+  ];
 }
